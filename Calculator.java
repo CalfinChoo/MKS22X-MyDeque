7cb1d2adb1779be514fa8d1d1f2053d3f6f1ac33
Calvin Chu <cchu00@stuy.edu>
2019-03-25 20:01:06 -0400

    fixed calculator

--- Calculator.java
+++ Calculator.java
@@ -1,22 +1,21 @@
-import java.util.*;
 public class Calculator{
     /*Evaluate a postfix expression stored in s.
      *Assume valid postfix notation, separated by spaces.
      */
     public static double eval(String s){
-      MyDeque<double> stack = new MyDeque();
+      MyDeque<Double> stack = new MyDeque<Double>();
       String scan = "";
       for (int i = 0; i < s.length(); i++) {
-        if (s.charAt(i) == ' ') {stack.addFirst(Integer.parseInt(scan)); scan = "";}
+        if (s.charAt(i) == ' ' && scan.length() != 0) {stack.addFirst(Double.parseDouble(scan)); scan = "";}
         else if (s.charAt(i) == '+') stack.addFirst(stack.removeFirst() + stack.removeFirst());
-        else if (s.charAt(i) == '-') stack.addFirst(stack.removeFirst() - stack.removeFirst());
+        else if (s.charAt(i) == '-') {double sec = stack.removeFirst(); stack.addFirst(stack.removeFirst() - sec);}
         else if (s.charAt(i) == '*') stack.addFirst(stack.removeFirst() * stack.removeFirst());
-        else if (s.charAt(i) == '/') stack.addFirst(stack.removeFirst() / stack.removeFirst());
-        else scan += s.charAt(i);
+        else if (s.charAt(i) == '/') {double sec = stack.removeFirst(); stack.addFirst(stack.removeFirst() / sec);}
+        else if (s.charAt(i) != ' ') scan += s.charAt(i);
       }
      return stack.removeFirst();
     }
     public static void main(String[] args) {
-      System.out.println(eval("2 4 71 8.5 + - * 5 +"));
+      //System.out.println(eval("2 4 71 8.5 + - * 5 +"));
     }
 }
